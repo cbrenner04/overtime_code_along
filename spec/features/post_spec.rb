d05be4ba6ea74ab41f1f28ffba403c5192c4209d
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-01-08 12:11:35 -0600

    Refactored Posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,28 +3,29 @@ require "rails_helper"
 
 describe "posts" do
   describe "index" do
+    before { visit posts_path }
+
     it "can be reached successfully" do
-      visit posts_path
       expect(status_code).to eq 200
     end
 
     it "has a title of Posts" do
-      visit posts_path
       expect(page).to have_text "Posts"
     end
   end
 
   describe "creation" do
+    before { visit new_post_path }
+
     it "has a new form that can be reached" do
-      visit new_post_path
       expect(status_code).to eq 200
     end
 
     it "can be created from new form page" do
-      visit new_post_path
       fill_in "post[date]", with: Date.today
       fill_in "post[rationale]", with: "foo"
       click_on "Save"
+
       expect(page).to have_text "foo"
     end
   end
