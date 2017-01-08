bd89c8a11055191b8784a121e7ad0d518e605d81
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-01-08 12:07:03 -0600

    TDD to create Posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -13,4 +13,19 @@ describe "posts" do
       expect(page).to have_text "Posts"
     end
   end
+
+  describe "creation" do
+    it "has a new form that can be reached" do
+      visit new_post_path
+      expect(status_code).to eq 200
+    end
+
+    it "can be created from new form page" do
+      visit new_post_path
+      fill_in "post[date]", with: Date.today
+      fill_in "post[rationale]", with: "foo"
+      click_on "Save"
+      expect(page).to have_text "foo"
+    end
+  end
 end
