5f9a7a5b1ead83d94c269b9af21dc260b8164b86
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-01-08 17:00:58 -0600

    Add new post button to navbar

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -25,7 +25,16 @@ describe "posts" do
     end
   end
 
-  describe "creation" do
+  describe "new" do
+    it "has a link from homepage" do
+      visit root_path
+      click_link("new_post_from_nav")
+
+      expect(status_code).to eq 200
+    end
+  end
+
+  describe "create" do
     before do
       visit new_post_path
     end
