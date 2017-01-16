26ce54d67ac186dcb22588eed49497d1090ba1c0
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-01-16 13:14:04 -0600

    Implemented enum for status into posts for approval workflow

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 # no doc
 class Post < ApplicationRecord
+  enum status: { submitted: 0, approved: 1, rejected: 2 }
   validates :date, :rationale, presence: true
   belongs_to :user
 end
