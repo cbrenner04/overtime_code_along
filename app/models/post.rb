08f75f3eed387b6daf99b3cc1365bb24c900e694
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-01-16 15:10:37 -0600

    Added custom scope for posts by user

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ApplicationRecord
   enum status: { submitted: 0, approved: 1, rejected: 2 }
   validates :date, :rationale, presence: true
   belongs_to :user
+
+  scope :posts_by, ->(user) { where(user: user) }
 end
