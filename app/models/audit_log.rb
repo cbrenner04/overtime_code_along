c5eb5bbe621e74456eb66fa586b9a1425244c5b4
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-01-17 19:45:41 -0600

    Integrated validations and defaults for audit log attrs

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -2,4 +2,14 @@
 # no doc
 class AuditLog < ApplicationRecord
   belongs_to :user
+
+  validates :user, :start_date, :status, presence: true
+
+  after_initialize :set_defaults
+
+  private
+
+  def set_defaults
+    self.start_date ||= Time.zone.today - 6.days
+  end
 end
--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -2,4 +2,37 @@
 require "rails_helper"
 
 RSpec.describe AuditLog, type: :model do
+  before { @audit_log = create :audit_log }
+
+  describe "creation" do
+    it "can be created" do
+      expect(@audit_log).to be_valid
+    end
+  end
+
+  describe "validations" do
+    it "is invalid when no user" do
+      @audit_log.user = nil
+
+      expect(@audit_log).to_not be_valid
+    end
+
+    it "is invalid when no status" do
+      @audit_log.status = nil
+
+      expect(@audit_log).to_not be_valid
+    end
+
+    it "is invalid when no start_date" do
+      @audit_log.start_date = nil
+
+      expect(@audit_log).to_not be_valid
+    end
+
+    it "has start_date equal to 6 days ago" do
+      new_audit_log = AuditLog.create(user: User.last)
+
+      expect(new_audit_log.start_date).to eq Time.zone.today - 6.days
+    end
+  end
 end
