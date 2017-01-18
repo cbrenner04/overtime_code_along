67d46913c2c4b24190253b40d09d0d86f5f2febc
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-01-17 19:48:17 -0600

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,13 +18,18 @@ User.create!(email: "admin@example.com",
 puts "1 Admin created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today,
+               rationale: "#{post} rationale content",
+               user_id: @user.id,
+               overtime_request: 2.5)
 end
 
 puts "100 posts have been created"
 
 100.times do |post|
-  AuditLog.create!(user: @user, status: 0, start_date: Time.zone.today - 6.days)
+  AuditLog.create!(user: @user,
+                   status: 0,
+                   start_date: Time.zone.today - 6.days)
 end
 
 puts "100 audit logs have been created"
