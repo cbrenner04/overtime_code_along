7732172f4f23680ac33cb51aa99903af767c39e6
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-01-18 07:52:17 -0600

    Blocked non admin users from access index page for audit logs

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -21,6 +21,13 @@ feature "Audit Log" do
       expect(page).to have_text(/BAR, FOO/)
     end
 
-    it "cannot be accessed by non admin"
+    it "cannot be accessed by non admin" do
+      logout(:user)
+      user = create :user
+      login_as(user, scope: :user)
+      visit audit_logs_path
+
+      expect(current_path).to eq root_path
+    end
   end
 end
